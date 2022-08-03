07a92494fb89443a2e3562fd401a413b537a9787
0fficerH0ps <hannahbrd15@gmail.com>
2022-08-02 21:42:30 -0500

    Added continuous movement

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -15,6 +15,14 @@ class Ship:
         # Start each new ship at the bottem center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Moement flag
+        self.moving_right = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
