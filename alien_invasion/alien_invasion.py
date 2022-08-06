bf670799903367d10fa871c08a380e9e15ce6461
0fficerH0ps <hannahbrd15@gmail.com>
2022-08-06 02:00:35 -0500

    fixed some oops

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -176,7 +176,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships_left.
             self.stats.ships_left -= 1
-            self.sb.srep_ships()
+            self.sb.prep_ships()
 
             # Get rid of any remaning aliens and bullets.
             self.aliens.empty()
--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -76,7 +76,7 @@ class Scoreboard:
 
     def show_score(self):
         """Draw scores, level, and ships to the screen."""
-        self.screen.bits(self.score_image, self.score_rect)
-        self.screen.bits(self.high_score_image, self.high_score_rect)
-        self.screen.bits(self.level_image, self.level_rect)
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
         self.ships.draw(self.screen)
